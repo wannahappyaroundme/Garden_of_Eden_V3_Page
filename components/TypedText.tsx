febd8749a8304c08cde091798e8794b6ad7652f2
Matthew <bu5119@hanyang.ac.kr>
2025-11-20 11:55:54 +0900

    Revise Words

--- components/TypedText.tsx
+++ components/TypedText.tsx
@@ -13,7 +13,7 @@ export default function TypedText() {
     // 언어별 단어 배열
     const words =
       locale === "ko"
-        ? ["동반자", "절친", "동료", "반쪽"]
+        ? ["동반자", "절친", "단짝", "반쪽"]
         : ["companion", "best friend", "soulmate", "another half"];
 
     if (el.current) {
